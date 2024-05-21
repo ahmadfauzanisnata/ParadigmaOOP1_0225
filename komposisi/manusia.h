f10a6890e10aa377cb7594269b9bea1f25af9fbf
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-21 15:28:29 +0700

    membuat kelas manusia,memberi akses ke public  dan membuat variabel name dan jantung serta membuat output dari kelas manusia

---
+++ komposisi/manusia.h
@@ -0,0 +1,16 @@
+#ifndef MANUSIA_H
+#define MANUSIA_H
+
+class manusia {
+    public:
+    stirng name;
+    jantung varJantung;
+
+    manusia(stirng pName) : name(pName) {
+            cout << name << " hidup\n";
+        }
+        ~manusia() {
+            cout << name << " mati\n";
+        }
+};
+#endif
