76d2cf3ebf719f673e3bce07b5c81a98e561eafa
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-05-21 17:21:56 +0700

    Menambahkan kelas pasien dengan asosiasi ke kelas dokter

--- inheritance.cpp
+++ inheritance.cpp
@@ -13,9 +13,11 @@ class orang {
         ~orang() {
             cout << "orang dihapus\n" << endl;
         }
+
         int jumlah(int a, int b) {
             return a + b;
         }
+        
 };
 
 class pelajar : public orang {
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+int main(){
+    pelajar siswa1("andi laksono", "SMAN 1 BANTUL");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
